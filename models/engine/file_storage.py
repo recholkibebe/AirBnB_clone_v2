a7e316f6714173c6e7d9ca660fcf81b5616d52ce
‘recholkibebe’ <recholkibebe@gmail.com>
2024-04-14 14:45:09 -0700

    tests files new

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -45,6 +45,10 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """ calls reload() for deserialising the JSON file to objects"""
+        self.reload()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the state class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """This is the class for State"""
+
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', back_populates='state', cascade='delete')
+
+    if models.storage_type != "db":
+        @property
+        def cities(self):
+            """
+            getter for cities
+            """
+        cities = []
+        insta = models.storage.all(City)
+        for value in insta.values():
+            if value.state_id == self.id:
+                cities.append(value)
+        return cities
